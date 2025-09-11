82fe5ea6339296a2d081b85dad3556a8e72e0294
Arya-Elyse <aryannadewitt2023@gmail.com>
2025-09-11 11:33:20 -0500

    First attempt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.room.common.jvm)
+    implementation(libs.room.runtime)
+    implementation(libs.room.runtime.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    annotationProcessor(libs.room.compiler)
 }
